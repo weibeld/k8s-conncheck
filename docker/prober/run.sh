3fd31c9f830208ed38e55038e6d6139100470b80
Daniel Weibel <danielmweibel@gmail.com>
2020-04-30 19:33:12 +0200

    Manual operation of controller and prober works

--- docker/prober/run.sh
+++ docker/prober/run.sh
@@ -1,6 +1,47 @@
 #!/bin/sh
 
-echo -e "Pods:\n$PODS"
-echo -e "Nodes:\n$NODES"
-echo -e "My node:\n$PROBER_NODE"
-echo -e "My IP:\n$PROBER_IP"
+is_reachable() {
+  if ping -q -c 1 -t 4 "$1" >/dev/null; then
+    echo true
+  else
+    echo false
+  fi
+}
+
+write() {
+  test=$1
+  ip=$2
+  success=$3
+  [[ "$success" = true ]] && icon="\xE2\x9C\x85" || icon="\xE2\x9D\x8C"
+  printf "$icon $test (=> $ip)\n"
+}
+
+# Self
+test="Self"
+ip=$PROBER_IP
+success=$(is_reachable "$ip")
+write "$test" "$ip" "$success"
+
+# Pod on same node
+test="Pod on same node"
+ip=$(echo "$PODS" | jq -r "map(select(.node==\"$PROBER_NODE\"))[0].ip")
+success=$(is_reachable "$ip")
+write "$test" "$ip" "$success"
+
+# Pod on different node
+test="Pod on different node"
+ip=$(echo "$PODS" | jq -r "map(select(.node!=\"$PROBER_NODE\"))[0].ip")
+success=$(is_reachable "$ip")
+write "$test" "$ip" "$success"
+
+# Same node
+test="Same node"
+ip=$(echo "$NODES" | jq -r "map(select(.name==\"$PROBER_NODE\"))[0] | .ip")
+success=$(is_reachable "$ip")
+write "$test" "$ip" "$success"
+
+# Different node
+test="Different node"
+ip=$(echo "$NODES" | jq -r "map(select(.name!=\"$PROBER_NODE\"))[0] | .ip")
+success=$(is_reachable "$ip")
+write "$test" "$ip" "$success"
