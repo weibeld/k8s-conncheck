5596ba52070b973f0204da6dee906e2510080e57
Daniel Weibel <danielmweibel@gmail.com>
2020-05-21 17:38:29 +0200

    Controller creates prober Deployments; prober Pod output results
    Revert to the controller Pod creating the prober Deployments, rather
    then them being defined in the conncheck.yaml manifest and then patched
    by the controller Pod (see disadvantages of this approach in commit
    message of previous commit).
    
    Additonally, the user-facing results are now output directly by the
    prober Pods, rather than being collected and output by the controller
    Pod as before. This removes a lot of implementation complexity while
    maintaining a comparable UX.
    
    To view the test results, the user can do:
    
      kubectl logs deployment/conncheck-prober
      kubectl logs deployment/conncheck-prober-hostnet
    
    Instead of:
    
      kubectl logs conncheck-controller
    
    as before.

--- docker/controller/run.sh
+++ docker/controller/run.sh
@@ -2,6 +2,10 @@
 
 #set -e
 
+log() {
+  echo -e "\e[94;1m$(date -Isec)\e[0m $@"
+}
+
 # Read URL of the API server from the kubelet's kubeconfig file
 api_server_url=$(yq read /etc/kubernetes/kubelet.conf 'clusters[0].cluster.server')
 
@@ -21,75 +25,6 @@ kubectlw() {
     "$@"
 }
 
-# Helper function for printing a log line consisting of a timestamp and message
-log() {
-  color_ts="\e[94;1m"
-  color_msg="\e[;1m"
-  echo -e "$color_ts$(date -Isec) $color_msg$1\e[0m"
-}
-
-# Invoked after the start of a prober Pod before the first test result is read.
-# Receives the following args:
-#   $1: name of the prober Pod
-#   $2: IP address of the prober Pod
-#   $3: name of the node on which the prober Pod is running
-#   $4: IP address of the node on which the prober Pod is running
-init() {
-  :
-}
-
-# Invoked for each test result of a prober Pod (a test consists of pinging
-# a target; a target may be a Pod or a node; a prober Pod performs a sequence
-# of tests with different targets). Receives the following args:
-#   $1: test result
-# The test result is a JSON object (formatted as a single line) with the
-# following fields:
-#   - test_id (string):     identifier of the type of test being performed
-#   - target_ip (string):   IP address of the target 
-#   - target_name (string): friendly name of the target
-#   - success (boolean):    whether the target could be reached or not
-process_test_result() {
-  test_id=$(echo "$1" | jq -r '.test_id')
-  target_ip=$(echo "$1" | jq -r '.target_ip')
-  target_name=$(echo "$1" | jq -r '.target_name')
-  success=$(echo "$1" | jq -r '.success')
-
-  case "$test_id" in
-    pod-self) msg="To self" ;;
-    pod-pod-local) msg="To pod on same node" ;;
-    pod-pod-remote) msg="To pod on different node" ;;
-    pod-node-local) msg="To own node" ;;
-    pod-node-remote) msg="To different node" ;;
-    pod-service) msg="To service" ;;
-    dns-internal) msg="DNS resolution of internal name" ;;
-    dns-external) msg="DNS resolution of external name" ;;
-    internet) msg="To internet"
-  esac
-
-  case "$success" in
-    true)
-      icon="\xE2\x9C\x85" 
-      color="\e[92;1m"
-      ;;
-    false)
-      icon="\xe2\x9b\x94"
-      color="\e[91;1m"
-      ;;
-  esac
-
-  [[ -n "$target_ip" ]] && sep=" " || sep=""
-  echo -e "$color  $icon $msg (\"$target_name\"$sep$target_ip)\e[0m"
-}
-
-# Invoked after all tests of a prober Pod have been completed. Receives no args.
-finalize() {
-  :
-}
-
-#==============================================================================#
-# Begin of execution
-#==============================================================================#
-
 log "Initialising..."
 
 # Wait until all target Pods are running
@@ -98,82 +33,85 @@ while ! daemonset=$(kubectlw get daemonset conncheck-target -o json) ||
   sleep 1
 done
 
-# TODO: wait until prober Pods are running
-
 # Query cluster topology
 pod_topology=$(kubectlw get pods -l "app=conncheck-target" -o json | jq -c '[.items[] | {name: .metadata.name, ip: .status.podIP, node: .spec.nodeName}]')
 node_topology=$(kubectlw get nodes -o json | jq -c '[.items[] | {name: .metadata.name, ip: .status.addresses[] | select(.type == "InternalIP") | .address}]')
 service_topology=$(kubectlw get service conncheck-service -o json | jq -c '{name: .metadata.name, ip: .spec.clusterIP, port: .spec.ports[0].port}')
 
-# Run two prober Deployments: one in the Pod network and one in the host network
-for run in pod_network host_network; do
-
-  case "$run" in
-    pod_network)
-      deployment_name=conncheck-prober
-      pod_label_key=app
-      pod_label_value=prober
-      ;;
-    host_network)
-      deployment_name=conncheck-prober-host-network
-      pod_label_key=app
-      pod_label_value=prober-host-network
-      ;;
-  esac
-
-  # Name of currently running prober Pod
-  old_pod=$(kubectlw get pods -l "$pod_label_key"="$pod_label_value" -o json | jq -r '.items[0].metadata.name')
-
-  # Patch Pod template of Deployment with topology information (restarts Pod)
-  log "Patching \"$deployment_name\"..."
-  patch=$(cat <<EOF
+template=$(mktemp)
+cat <<EOF >$template
+apiVersion: apps/v1
+kind: Deployment
+metadata:
 spec:
+  replicas: 1
+  selector:
+    matchLabels:
   template:
+    metadata:
+      labels:
     spec:
       containers:
-        - name: k8s-conncheck-prober
-          env:
-            - name: PODS
-              value: '$pod_topology'
-            - name: NODES
-              value: '$node_topology'
-            - name: SERVICE 
-              value: '$service_topology'
+      - image: weibeld/k8s-conncheck-prober
+        name: k8s-conncheck-prober
+        imagePullPolicy: Always
+        env:
+          - name: PODS
+            value: '$pod_topology'
+          - name: NODES
+            value: '$node_topology'
+          - name: SERVICE
+            value: '$service_topology'
+          - name: SELF_POD_NAME
+            valueFrom:
+              fieldRef:
+                fieldPath: metadata.name
+          - name: SELF_POD_IP
+            valueFrom:
+              fieldRef:
+               fieldPath: status.podIP
+          - name: SELF_NODE_NAME
+            valueFrom:
+              fieldRef:
+               fieldPath: spec.nodeName
+          - name: SELF_NODE_IP
+            valueFrom:
+              fieldRef:
+               fieldPath: status.hostIP
 EOF
-)
-  kubectlw patch deployment "$deployment_name" -p "$patch" >/dev/null
 
-  # Get name of the newly created Pod
-  while true; do
-    pod_name=$(kubectlw get pods -l "$pod_label_key"="$pod_label_value" -o json | jq -r ".items[] | select(.metadata.name!=\"$old_pod\") | .metadata.name")
-    [[ -n "$pod_name" ]] && break || sleep 1
-  done
+# Run two prober Deployments: one in the Pod network and one in the host network
+for name in conncheck-prober conncheck-prober-hostnet; do
 
-  # Wait until the new Pod is running 
-  while [[ "$(kubectlw get pod "$pod_name" -o jsonpath='{.status.phase}')" != Running ]]; do sleep 1; done
+  yq write -i "$template" metadata.name "$name"
+  yq write -i "$template" spec.selector.matchLabels.app "$name"
+  yq write -i "$template" spec.template.metadata.labels.app "$name"
 
-  # Query details of the new Pod
-  pod_manifest=$(kubectlw get pod "$pod_name" -o json)
-  pod_ip=$(echo "$pod_manifest" | jq -r '.status.podIP')
-  node_name=$(echo "$pod_manifest" | jq -r '.spec.nodeName')
-  node_ip=$(echo "$pod_manifest" | jq -r '.status.hostIP')
-  log "Running tests on Pod \"$pod_name\" $pod_ip (node \"$node_name\" $node_ip)"
+  if [[ "$name" = conncheck-prober-hostnet ]]; then
+    yq write -i "$template" spec.template.spec.hostNetwork true
+    yq write -i "$template" spec.template.spec.dnsPolicy ClusterFirstWithHostNet    
+  fi
 
-  # Invoke 'init' callback
-  init "$pod_name" "$pod_ip" "$node_name" "$node_ip"
-
-  # Read test results from Pod and invoke 'process_test_result' callbacks
-  kubectlw logs -f "$pod_name" 2>/dev/null | while read -r line; do 
-    if [[ "$line" = EOF ]]; then
-      pkill kubectl  # Caution: if 'set -e' is set, this causes script to exit
-      break
-    fi
-    process_test_result "$line"
-  done
-
-  # Invoke 'finalize' callback
-  finalize
+  log "Creating Deployment \"$name\"..."
+  kubectlw create -f "$template" >/dev/null
 
 done
-
-sleep infinity
+ 
+#  # Patch Pod template of Deployment with topology information (restarts Pod)
+#  log "Patching \"$deployment_name\"..."
+#  patch=$(cat <<EOF
+#spec:
+#  template:
+#    spec:
+#      containers:
+#        - name: k8s-conncheck-prober
+#          env:
+#            - name: PODS
+#              value: '$pod_topology'
+#            - name: NODES
+#              value: '$node_topology'
+#            - name: SERVICE 
+#              value: '$service_topology'
+#EOF
+#)
+#  kubectlw patch deployment "$deployment_name" -p "$patch" >/dev/null
--- docker/prober/run.sh
+++ docker/prober/run.sh
@@ -1,101 +1,159 @@
 #!/bin/sh
 
-if [[ -z "$PODS" ]]; then
-  echo "$SELF_POD_NAME: not launching tests"
+#------------------------------------------------------------------------------#
+# Output functions
+#------------------------------------------------------------------------------#
+
+log() {
+  echo -ne "\e[94;1m$(date -Isec)\e[0m"
+  for a in "$@"; do
+    echo -ne " \e[;1m$a\e[0m"
+  done
+  echo
+}
+
+format_result() {
+  description=$1
+  target_ip=$2
+  target_name=$3
+  success=$4
+
+  case "$success" in
+    true)
+      success_color="\e[92;1m"
+      success_msg=SUCCESS
+      ;;
+    false)
+      success_color="\e[91;1m"
+      success_msg=FAILURE
+      ;;
+  esac
+
+  [[ -n "$target_ip" ]] && sep=" " || sep=""
+
+  echo -e "$success_color$success_msg\e[0m $description (\"$target_name\"$sep$target_ip)"
+}
+
+#------------------------------------------------------------------------------#
+# Connectivity check functions
+#------------------------------------------------------------------------------#
+
+icmp() {
+  ip=$1
+  # If using the -t option, the requests fail when pining a host in the internet
+  ping -c 1 "$ip" 1>/dev/null 2>&1
+}
+
+tcp() {
+  ip=$1
+  port=$2
+  nc -z -w 3 "$ip" "$port" 1>/dev/null 2>&1
+}
+
+dns() {
+  name=$1
+  nslookup "$name" 1>/dev/null 2>&1
+}
+
+#------------------------------------------------------------------------------#
+# Begin of execution
+#------------------------------------------------------------------------------#
+
+log "Pod \"$SELF_POD_NAME\" $SELF_POD_IP (node \"$SELF_NODE_NAME\" $SELF_NODE_IP)"
+
+description="Self"
+target_ip=$SELF_POD_IP
+target_name=$SELF_POD_NAME
+if icmp "$target_ip"; then
+  success=true
 else
-  icmp() {
-    ip=$1
-    # If using the -t option, the requests fail when pining a host in the internet
-    ping -c 1 "$ip" 1>/dev/null 2>&1
-  }
-
-  tcp() {
-    ip=$1
-    port=$2
-    nc -z -w 3 "$ip" "$port" 1>/dev/null 2>&1
-  }
-
-  dns() {
-    name=$1
-    nslookup "$name" 1>/dev/null 2>&1
-  }
-
-  write_result() {
-    test_id=$1
-    target_ip=$2
-    target_name=$3
-    success=$4
-    printf '{"test_id":"%s","target_ip":"%s","target_name":"%s","success":%s}\n' "$test_id" "$target_ip" "$target_name" "$success"
-  }
-
-  # Self
-  test_id=pod-self
-  target_ip=$SELF_POD_IP
-  target_name=$SELF_POD_NAME
-  icmp "$target_ip" && success=true || success=false
-  write_result "$test_id" "$target_ip" "$target_name" "$success"
-
-  # Pod on same node
-  test_id=pod-pod-local
-  tmp=$(echo "$PODS" | jq -r "map(select(.node==\"$SELF_NODE_NAME\"))[0] | .ip + \",\" + .name")
-  target_ip=$(echo "$tmp" | cut -d , -f 1)
-  target_name=$(echo "$tmp" | cut -d , -f 2)
-  icmp "$target_ip" && success=true || success=false
-  write_result "$test_id" "$target_ip" "$target_name" "$success"
-
-  # Pod on different node
-  test_id=pod-pod-remote
-  tmp=$(echo "$PODS" | jq -r "map(select(.node!=\"$SELF_NODE_NAME\"))[0] | .ip + \",\" + .name")
-  target_ip=$(echo "$tmp" | cut -d , -f 1)
-  target_name=$(echo "$tmp" | cut -d , -f 2)
-  icmp "$target_ip" && success=true || success=false
-  write_result "$test_id" "$target_ip" "$target_name" "$success"
-
-  # Same node
-  test_id=pod-node-local
-  tmp=$(echo "$NODES" | jq -r "map(select(.name==\"$SELF_NODE_NAME\"))[0] | .ip + \",\" + .name")
-  target_ip=$(echo "$tmp" | cut -d , -f 1)
-  target_name=$(echo "$tmp" | cut -d , -f 2)
-  icmp "$target_ip" && success=true || success=false
-  write_result "$test_id" "$target_ip" "$target_name" "$success"
-
-  # Different node
-  test_id=pod-node-remote
-  tmp=$(echo "$NODES" | jq -r "map(select(.name!=\"$SELF_NODE_NAME\"))[0] | .ip + \",\" + .name")
-  target_ip=$(echo "$tmp" | cut -d , -f 1)
-  target_name=$(echo "$tmp" | cut -d , -f 2)
-  icmp "$target_ip" && success=true || success=false
-  write_result "$test_id" "$target_ip" "$target_name" "$success"
-
-  # Service
-  test_id=pod-service
-  target_ip=$(echo "$SERVICE" | jq -r .ip)
-  target_port=$(echo "$SERVICE" | jq -r .port)
-  target_name=$(echo "$SERVICE" | jq -r .name)
-  tcp "$target_ip" "$target_port" && success=true || success=false
-  write_result "$test_id" "$target_ip" "$target_name" "$success"
-
-  # Internal DNS
-  test_id=dns-internal
-  target_name=$(echo "$SERVICE" | jq -r .name)
-  dns "$target_name" && success=true || success=false
-  write_result "$test_id" "" "$target_name" "$success"
-
-  # External DNS
-  test_id=dns-external
-  target_name=kubernetes.io
-  dns "$target_name" && success=true || success=false
-  write_result "$test_id" "" "$target_name" "$success"
-
-  # Internet
-  test_id=internet
-  target_name=a.root-servers.net
-  target_ip=198.41.0.4
-  icmp "$target_ip" && success=true || success=false
-  write_result "$test_id" "$target_ip" "$target_name" "$success"
-
-  echo EOF
-
+  success=false
 fi
+log $(format_result "$description" "$target_ip" "$target_name" "$success")
+
+description="Pod on local node"
+target=$(echo "$PODS" | jq "map(select(.node==\"$SELF_NODE_NAME\")) | first")
+target_ip=$(echo "$target" | jq -r .ip)
+target_name=$(echo "$target" | jq -r .name)
+if icmp "$target_ip"; then
+  success=true
+else
+  success=false
+fi
+log $(format_result "$description" "$target_ip" "$target_name" "$success")
+
+description="Pod on remote node"
+target=$(echo "$PODS" | jq "map(select(.node!=\"$SELF_NODE_NAME\")) | first")
+target_ip=$(echo "$target" | jq -r .ip)
+target_name=$(echo "$target" | jq -r .name)
+if icmp "$target_ip"; then
+  success=true
+else
+  success=false
+fi
+log $(format_result "$description" "$target_ip" "$target_name" "$success")
+
+description="Local node"
+target=$(echo "$NODES" | jq "map(select(.name==\"$SELF_NODE_NAME\")) | first")
+target_ip=$(echo "$target" | jq -r .ip)
+target_name=$(echo "$target" | jq -r .name)
+if icmp "$target_ip"; then
+  success=true
+else
+  success=false
+fi
+log $(format_result "$description" "$target_ip" "$target_name" "$success")
+
+description="Remote node"
+target=$(echo "$NODES" | jq "map(select(.name!=\"$SELF_NODE_NAME\")) | first")
+target_ip=$(echo "$target" | jq -r .ip)
+target_name=$(echo "$target" | jq -r .name)
+if icmp "$target_ip"; then
+  success=true
+else
+  success=false
+fi
+log $(format_result "$description" "$target_ip" "$target_name" "$success")
+
+description="Service"
+target_ip=$(echo "$SERVICE" | jq -r .ip)
+target_name=$(echo "$SERVICE" | jq -r .name)
+target_port=$(echo "$SERVICE" | jq -r .port)
+if tcp "$target_ip" "$target_port"; then
+  success=true
+else
+  success=false
+fi
+log $(format_result "$description" "$target_ip" "$target_name" "$success")
+
+description="Internet"
+target_ip=198.41.0.4
+target_name=a.root-servers.net
+if icmp "$target_ip"; then
+  success=true
+else
+  success=false
+fi
+log $(format_result "$description" "$target_ip" "$target_name" "$success")
+
+description="Internal DNS resolution"
+target_ip=""
+target_name=$(echo "$SERVICE" | jq -r .name)
+if dns "$target_name"; then
+  success=true
+else
+  success=false
+fi
+log $(format_result "$description" "$target_ip" "$target_name" "$success")
+
+description="External DNS resolution"
+target_ip=""
+target_name=kubernetes.io
+if dns "$target_name"; then
+  success=true
+else
+  success=false
+fi
+log $(format_result "$description" "$target_ip" "$target_name" "$success")
 
 sleep infinity
